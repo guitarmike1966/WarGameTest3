bc0318fd030742ed5c6ad89648431bd7829eff3e
Mike O'Connell <guitarmike1966@gmail.com>
2020-05-05 23:05:06 -0500

    Initial Commit

---
+++ WarGameTest3/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WarGameTest3
+//
+//  Created by Michael O'Connell on 5/5/20.
+//  Copyright © 2020 Michael O'Connell. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
